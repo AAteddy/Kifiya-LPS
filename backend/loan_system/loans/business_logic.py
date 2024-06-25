1d1e51697b5407d0ce8fa0d4c624daca4e7c30c5
Tedros Alem <tedsaasfaha@gmail.com>
2024-06-25 19:31:20 +0300

    [loans/business_logic.py file created] implementing LPS business rules

---
+++ backend/loan_system/loans/business_logic.py
@@ -0,0 +1,14 @@
+from .models import LoanApplication, LoanRepayment
+from django.utils import timezone
+
+
+def validate_loan_application(loan_application):
+    errors = []
+    borrower = loan_application.borrower
+    if borrower.credit_score < 600:
+        errors.append("Credit score too low")
+    if borrower.annual_income <= 0:
+        errors.append("Invalid annual income")
+    if borrower.debt_to_income_ratio > 0.4:
+        errors.append("Debt to income ratio too high")
+    return errors
