9e5a9dc6c3fd96700c43a836bf9a91d93215e46b
Tedros Alem <tedsaasfaha@gmail.com>
2024-06-25 19:22:59 +0300

    [serializers] created serializers for all instances

--- backend/loan_system/loans/serializers.py
+++ backend/loan_system/loans/serializers.py
@@ -6,3 +6,17 @@ class BorrowerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Borrower
         fields = "__all__"
+
+
+class LoanApplicationSerializer(serializers.ModelSerializer):
+    borrower = BorrowerSerializer()
+
+    class Meta:
+        model = LoanApplication
+        fields = "__all__"
+
+
+class LoanRepaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LoanRepayment
+        fields = "__all__"
