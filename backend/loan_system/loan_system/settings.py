a508837f6aa0f353c8858332860f169ac81b3d6f
Tedros Alem <tedsaasfaha@gmail.com>
2024-06-26 13:19:16 +0300

    [update on settings.py] loans.middleware.ExceptionMiddleware added to MIDDLEWARE

--- backend/loan_system/loan_system/settings.py
+++ backend/loan_system/loan_system/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "loans.middleware.ExceptionMiddleware",
 ]
 
 ROOT_URLCONF = "loan_system.urls"
